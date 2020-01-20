6c1518efe8928327db38df1d2b768dc14310f398
munyaaa <jessicajimantoro@gmail.com>
2020-01-20 21:17:44 +0700

    add seeder

--- database/seeds/RequestDetailTableSeeder.php
+++ database/seeds/RequestDetailTableSeeder.php
@@ -12,17 +12,6 @@ class RequestDetailTableSeeder extends Seeder
      */
     public function run()
     {
-        /*
-            $table->bigIncrements('id');
-            $table->bigInteger('request_id')->unsigned();
-            $table->foreign('request_id')->references('id')->on('requests');
-            $table->bigInteger('item_id')->unsigned();
-            $table->foreign('item_id')->references('id')->on('items');
-            $table->enum('size', array('S', 'M', 'L', 'XL', 'XXL', 'XXXL'));
-            $table->string('color');
-            $table->double('priceSnapshot', 10, 2);
-            $table->timestamps();
-        */
         DB::table('request_details')->insert([
             [
                 'request_id' => 1,
--- database/seeds/RequestTableSeeder.php
+++ database/seeds/RequestTableSeeder.php
@@ -12,16 +12,6 @@ class RequestTableSeeder extends Seeder
      */
     public function run()
     {
-        /*
-            $table->bigIncrements('id');
-            $table->bigInteger('buyer_id')->unsigned();
-            $table->foreign('buyer_id')->references('id')->on('users');
-            $table->bigInteger('seller_id')->unsigned();
-            $table->foreign('seller_id')->references('id')->on('users');
-            $table->enum('status', array('WAITING_FOR_APPROVAL', 'APPROVED', 'DECILINED', 'WORK_IN_PROGRESS', 'ON SHIPPING', 'DONE'));
-            $table->dateTime('approval_at');
-            $table->timestamps();
-        */
         DB::table('requests')->insert([
             [
                 'buyer_id' => 1,
